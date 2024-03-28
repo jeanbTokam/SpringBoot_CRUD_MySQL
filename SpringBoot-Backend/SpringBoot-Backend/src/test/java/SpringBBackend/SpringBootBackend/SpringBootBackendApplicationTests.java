534eefe3839527c6afcdec64e5965b7df7881efc
jbtokam <jeanbTokam@github.com>
2024-03-28 13:36:16 -0400

    Added Spring Boot

---
+++ SpringBoot-Backend/SpringBoot-Backend/src/test/java/SpringBBackend/SpringBootBackend/SpringBootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBBackend.SpringBootBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
