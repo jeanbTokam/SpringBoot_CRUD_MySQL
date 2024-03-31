e1d079de50094ff93790cb8984a81b607584c8a2
jbtokam <jeanbTokam@github.com>
2024-03-31 00:39:25 -0400

    Added create resources

--- SpringBoot-Backend/SpringBoot-Backend/src/main/java/SpringBBackend/SpringBootBackend/Controller/EmployeeController.java
+++ SpringBoot-Backend/SpringBoot-Backend/src/main/java/SpringBBackend/SpringBootBackend/Controller/EmployeeController.java
@@ -3,12 +3,11 @@ package SpringBBackend.SpringBootBackend.Controller;
 import SpringBBackend.SpringBootBackend.Entity.Employee;
 import SpringBBackend.SpringBootBackend.Repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
@@ -20,5 +19,11 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
        return employeeRepository.findAll();
+    }
+
+    // create employee
+    @PostMapping("/employees")
+    public Employee CreateEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
     }
 }
